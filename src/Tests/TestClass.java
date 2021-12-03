2e0b1f452b1597a29d848a3322ba7da5ebccecb3
skyg547 <skyg547@gmail.com>
2021-12-03 16:27:22 +0900

    Write TestCode

---
+++ src/Tests/TestClass.java
@@ -0,0 +1,15 @@
+package Tests;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+public class TestClass {
+
+    @Test
+    public void method() {
+        // org.junit.Assert.assertTrue( new ArrayList<>().isEmpty() );
+        assertEquals(1, 1);
+    }
+}
