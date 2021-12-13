3da7d57b1779dcb2d051512df8d08c669a4e8568
skyg547 <skyg547@gmail.com>
2021-12-13 10:29:43 +0900

    portal 연결

--- src/main/xml.java
+++ src/main/xml.java
@@ -7,7 +7,21 @@ import java.net.URL;
 public class xml {
 
     public static void main(String[] args) {
+        URL portalUrl = null;
 
+        try {
+            portalUrl =  new URL("http://portaldev.daiso.co.kr:8080/ikep/rest/comb/autoappr/report");  
+            
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+        System.out.println("Protocol : " + portalUrl.getProtocol());   
+        System.out.println("host : " + portalUrl.getHost());   
+        System.out.println("path : " + portalUrl.getPath());   
+        System.out.println("file : " + portalUrl.getFile());   
+        System.out.println("port : " + portalUrl.getPort());         
+        
         // type Post
         // url: "http://portaldev.daiso.co.kr:8080/ikep/rest/comb/autoappr/report", 개발
         // url: "http://portal.daiso.co.kr/rest/comb/autoappr/report",
@@ -27,7 +41,8 @@ public class xml {
         
         /* 데이터 구조 
         "<root>"
-         '<dataset01><mobslipno>' + $.fn.limitSpecialCharacters(response.sql1[0].MOB_SLIP_NO) + '</mobslipno>' +
+         '<dataset01>
+         <mobslipno>' + $.fn.limitSpecialCharacters(response.sql1[0].MOB_SLIP_NO) + '</mobslipno>' +
 				'<regusernm>' + $.fn.limitSpecialCharacters(response.sql1[0].USER_NM) + '</regusernm>' +
 				'<regdeptnm>' + $.fn.limitSpecialCharacters(response.sql1[0].DEPT_NM) + '</regdeptnm>' +
 				'<slipamt>' + $.fn.limitSpecialCharacters($.fn.newStrfunction(response.sql1[0].SLIP_AMT)) + '</slipamt>' +
@@ -35,7 +50,8 @@ public class xml {
 				'<acntdt>' + $.fn.limitSpecialCharacters(response.sql1[0].ACNT_DT.substr(0, 4)) + '-' + response.sql1[0].ACNT_DT.substr(4, 2) + '-' + response.sql1[0].ACNT_DT.substr(6, 2) + '</acntdt>' +
 				'<regdtm>' + $.fn.limitSpecialCharacters(response.sql1[0].REG_DTM.substr(0, 10)) + '</regdtm></dataset01>';
                
-                <dataset02>'<record>' +
+                <dataset02>
+                '<record>' +
                 '<slipsno>' + $.fn.limitSpecialCharacters(element.SLIP_SNO) + '</slipsno>' +
                 '<essnccntn>' + $.fn.limitSpecialCharacters(element.ESSNC_CNTN) + '</essnccntn>' +
                 '<accd>' + $.fn.limitSpecialCharacters(element.AC_CD) + '</accd>' +
@@ -56,7 +72,8 @@ public class xml {
 					'<svos>' + $.fn.limitSpecialCharacters($.fn.newStrfunction(element.SVOS)) + '</svos>' +
 					'<vatamt>' + $.fn.limitSpecialCharacters($.fn.newStrfunction(element.VAT_AMT)) + '</vatamt>' +
 					'</record>';
-                    '</dataset04>'
+
+                '</dataset04>'
 
                     '<dataset05><amt>' + $.fn.limitSpecialCharacters($.fn.newStrfunction(response.sql5[0].AC_AMT)) + '</amt></dataset05>';
                    
@@ -65,6 +82,46 @@ public class xml {
                     
                 
         String xmlDatasStringf = "";
+
+        xmlDatasStringf += "<root>";
+
+        xmlDatasStringf += "<dataset01>";
+        xmlDatasStringf += "<mobslipno>100041222282</mobslipno>";
+        xmlDatasStringf += "<regusernm>김성욱</regusernm>";
+        xmlDatasStringf += "<regdeptnm>영업팀</regdeptnm>";
+        xmlDatasStringf += "<slipamt>1,000,000</slipamt>";
+        xmlDatasStringf += "<essnccntn>삼성카드</essnccntn>";
+        xmlDatasStringf += "<acntdt>2017-01-01</acntdt>";
+        xmlDatasStringf += "<regdtm>2017-01-01</regdtm></dataset01>";
+
+        xmlDatasStringf += "<dataset02>";
+        xmlDatasStringf += "<record>";
+        xmlDatasStringf += "<slipsno>1</slipsno>";
+        xmlDatasStringf += "<essnccntn>삼성카드</essnccntn>";
+        xmlDatasStringf += "<accd>1</accd>";
+        xmlDatasStringf += "<acamt>1,000,000</acamt>";
+        xmlDatasStringf += "<svos>1,000,000</svos>";
+        xmlDatasStringf += "<vatamt>0</vatamt>";
+        xmlDatasStringf += "</record>";
+        xmlDatasStringf += "</dataset02>";
+
+        xmlDatasStringf += "<dataset03><amt>1,000,000</amt></dataset03>";
+
+        xmlDatasStringf += "<dataset04>";
+        xmlDatasStringf += "<record>";
+        xmlDatasStringf += "<slipsno>1</slipsno>";
+        xmlDatasStringf += "<essnccntn>삼성카드</essnccntn>";
+        xmlDatasStringf += "<accd>1</accd>";
+        xmlDatasStringf += "<acamt>1,000,000</acamt>";
+        xmlDatasStringf += "<svos>1,000,000</svos>";   
+        xmlDatasStringf += "<vatamt>0</vatamt>";
+        xmlDatasStringf += "</record>";
+        xmlDatasStringf += "</dataset04>";
+
+        xmlDatasStringf += "<dataset05><amt>1,000,000</amt></dataset05>";
+
+        xmlDatasStringf += "</root>";
+
 
         try {
 
