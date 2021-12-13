53f3861c8ef1b5162d52326017dd138338764125
skyg547 <skyg547@gmail.com>
2021-12-13 10:31:15 +0900

    get portal url

--- src/main/xml.java
+++ src/main/xml.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
@@ -11,7 +13,10 @@ public class xml {
 
         try {
             portalUrl =  new URL("http://portaldev.daiso.co.kr:8080/ikep/rest/comb/autoappr/report");  
-            
+            BufferedReader in = new BufferedReader(new InputStreamReader(portalUrl.openStream()));
+    
+            String line = null;
+            wh
         } catch (Exception e) {
             //TODO: handle exception
         }
@@ -20,7 +25,9 @@ public class xml {
         System.out.println("host : " + portalUrl.getHost());   
         System.out.println("path : " + portalUrl.getPath());   
         System.out.println("file : " + portalUrl.getFile());   
-        System.out.println("port : " + portalUrl.getPort());         
+        System.out.println("port : " + portalUrl.getPort());     
+        
+        
         
         // type Post
         // url: "http://portaldev.daiso.co.kr:8080/ikep/rest/comb/autoappr/report", 개발
