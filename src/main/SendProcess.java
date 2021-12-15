986afbab3eaba734e8b1316975af44c15eebd2b1
skyg547 <skyg547@gmail.com>
2021-12-15 13:54:15 +0900

    make moc data

--- src/main/SendProcess.java
+++ src/main/SendProcess.java
@@ -48,7 +48,6 @@ public class SendProcess {
      * SLIP_ST_CD=1}];
      */
 
-    List<LinkedHashMap<String, Object>> sql2 = null;
 
     // [
     // {SLIP_SNO=1,
@@ -60,44 +59,89 @@ public class SendProcess {
     // SUP_NM=GS25 제주노형점,
     // MNG_DT=20211023}];
 
-    List<LinkedHashMap<String, Object>> sql3 = null;
     // [{AC_AMT=1400}];
 
-    List<LinkedHashMap<String, Object>> sql4 = null;
     // [];
 
-    List<LinkedHashMap<String, Object>> sql5 = null;
     // [{AC_AMT=0}];
 
     public Document makeDocument() {
 
+        //given
+
         List<LinkedHashMap<String, Object>> sql1 = new ArrayList<LinkedHashMap<String, Object>>();
+        List<LinkedHashMap<String, Object>> sql2 = new ArrayList<LinkedHashMap<String, Object>>();
+        List<LinkedHashMap<String, Object>> sql3 = new ArrayList<LinkedHashMap<String, Object>>();
+        List<LinkedHashMap<String, Object>> sql4 = new ArrayList<LinkedHashMap<String, Object>>();
+        List<LinkedHashMap<String, Object>> sql5 = new ArrayList<LinkedHashMap<String, Object>>();
 
-        String[] a = new String[] { "MOB_SLIP_NO", "USER_NM", "DEPT_NM", "SLIP_AMT", "ESSNC_CNTN", "ACNT_DT", "REG_DTM",
-                "SLIP_ST_CD" };
-        Object[] b = new Object[] { "MOBSL2021120388", "정용진", "관리정보팀", 1400, "정용진_신청(모바일_경비신청)", 20211023,
-                "2021-12-15 11:23:33.0", 1 };
-        Object[] c = new Object[] { "MOBSL2021120388", "정용진", "관리정보팀", 3333, "정용진_신청(모바일_경비신청)", 20211023,
-                "2021-12-15 11:23:33.0", 1 };
-        Object[] d = new Object[] { "MOBSL2021120388", "정용진", "관리정보팀", 4444, "정용진_신청(모바일_경비신청)", 20211023,
-                "2021-12-15 11:23:33.0", 1 };
-
-
-
-
-
+        
         LinkedHashMap<String, Object> map = new LinkedHashMap<String, Object>();
 
-        for (int i = 0; i < a.length; i++) {
-            map.put(a[i], b[i]);
-            // System.out.println(map.get(a[i]));
+        String[] key1 = new String[] { "MOB_SLIP_NO", "USER_NM", "DEPT_NM", "SLIP_AMT", "ESSNC_CNTN", "ACNT_DT", "REG_DTM",
+                "SLIP_ST_CD" };
+
+        Object[] data1 = new Object[] { "MOBSL2021120388", "정용진", "관리정보팀", 1400, "정용진_신청(모바일_경비신청)", 20211023,
+                "2021-12-15 11:23:33.0", 1 };
+        Object[] data2 = new Object[] { "MOBSL2021120388", "정용진", "관리정보팀", 3333, "정용진_신청(모바일_경비신청)", 20211023,
+                "2021-12-15 11:23:33.0", 1 };
+        Object[] data3 = new Object[] { "MOBSL2021120388", "정용진", "관리정보팀", 4444, "정용진_신청(모바일_경비신청)", 20211023,
+                "2021-12-15 11:23:33.0", 1 };
+
+
+        List<Object[]> list1 = new ArrayList<Object[]>();
+
+        list1.add(data1);
+        list1.add(data2);
+        list1.add(data3);
+
+        for (Object[] obj : list1) {
+            map = new LinkedHashMap<String, Object>();
+            for (int i = 0; i < key1.length; i++) {
+                map.put(key1[i], obj[i]);
+            }
+            sql1.add(map);
         }
 
-        sql1.add(map);
+        String[] key2 = new String[] { "SLIP_SNO", "ESSNC_CNTN", "AC_AMT", "AC_CD", "VAT_AMT", "SVOS", "SUP_NM", "MNG_DT" };
 
-        for (String key : map.keySet()) {
-            // System.out.println(map.size());
-            System.out.println(key.toLowerCase().replaceAll("_", "") + '=' + map.get(key));
+        Object[] data4 = new Object[] { 1, "일 석", 1400, "복리후생비_실비식대_석식", 127, 1273, "GS25 제주노형점", 20211023 };
+
+        map = new LinkedHashMap<String, Object>();
+        for (int i = 0; i < key2.length; i++) {
+            map.put(key2[i], data4[i]);
+        }
+        sql2.add(map);
+
+        String[] key3 = new String[] { "AC_AMT" };
+
+        Object[] data5 = new Object[] { 1400 };
+
+        map = new LinkedHashMap<String, Object>();
+        for (int i = 0; i < key3.length; i++) {
+            map.put(key3[i], data5[i]);
+        }
+        sql3.add(map);
+
+
+        String[] key4 = new String[] { "AC_AMT" };
+
+        Object[] data6 = new Object[] { 0 };
+        
+        map = new LinkedHashMap<String, Object>();
+        for (int i = 0; i < key4.length; i++) {
+            map.put(key4[i], data6[i]);
+        }
+        sql5.add(map);
+
+
+        for(LinkedHashMap<String, Object> maps : sql1){
+
+            for (String key : maps.keySet()) {
+                // System.out.println(map.size());
+                System.out.println(key.toLowerCase().replaceAll("_", "") + '=' + maps.get(key));
+    
+            }
 
         }
 
