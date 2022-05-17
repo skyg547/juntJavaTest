2a305a1d1b5a7b540f49f3251769213b1e07af82
skyg547 <skyg547@gmail.com>
2022-05-17 10:08:30 +0900

    Boolean toString Test

---
+++ src/main/BooleanTest.java
@@ -0,0 +1,14 @@
+package main;
+
+public class BooleanTest{
+    public static void main(String[] args) {
+        System.out.println("============================================");
+
+        Boolean t = true;
+        Boolean f = false;
+
+        System.out.println(t.toString());
+        System.out.println(f.toString());
+    }
+
+}
