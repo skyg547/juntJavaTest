39071b56ba80f3d4260ff5ecff17e1828af262ed
skyg547 <skyg547@gmail.com>
2023-07-10 22:51:58 +0900

    [Dollar] fail Dollar and product

--- src/Tests/DollarTest.java
+++ src/Tests/DollarTest.java
@@ -5,11 +5,14 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class DollarTest {
-    @Test
-    public void testMuultiplication() {
-        Dollar five = new Dollar(5);
-        five.times(2);
-        Assert.assertEquals(10, five.amount);
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		Dollar product = five.times(2);
+		Assert.assertEquals(10, product.amount);
+		product = five.times(3);
+		Assert.assertEquals(15, product.amount);
 
-    }
+
+	}
 }
